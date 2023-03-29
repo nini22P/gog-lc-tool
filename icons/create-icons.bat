06e2229203ccc34c1c97ca35813019ffeaf4ec2f
22 <60903333+nini22P@users.noreply.github.com>
2023-03-29 21:52:52 +0800

    Add icons, support jp_usd and ru_rub.

---
+++ icons/create-icons.bat
@@ -0,0 +1,4 @@
+ffmpeg -i icon.png -vf scale=128:128 icon-128x128.png -y
+ffmpeg -i icon.png -vf scale=48:48 icon-48x48.png -y
+ffmpeg -i icon.png -vf scale=32:32 icon-32x32.png -y
+ffmpeg -i icon.png -vf scale=16:16 icon-16x16.png -y
